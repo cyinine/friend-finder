ebcd0ff2ec340d1b679224de82d537d253e189dd
do ck <dock.tedford@gmail.com>
2020-02-05 23:19:36 -0500

    finished app

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+    {
+        name:"bill murry",
+        photo:" https://fillmurray.com/g/200/300",
+        scores: [5,1,4,4,5,1,2,4,1]
+    },
+    {
+        name:"jason patrick",
+        photo:"https://img.buzzfeed.com/buzzfeed-static/static/2017-10/13/14/asset/buzzfeed-prod-fastlane-01/sub-buzz-8818-1507917865-2.jpg?crop=274%3A410%3B132%2C0&downsize=400:*&output-format=auto&output-quality=auto",
+        scores: [1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name:"nicholas cage",
+        photo:" https://www.placecage.com/c/200/300",
+        scores: [5,4,3,2,1,2,3,1,4,5]
+    },
+    {
+        name:"steven segall",
+        photo:"https://stevensegallery.com/200/300",
+        scores: [1,2,3,2,1,1,2,1,3,2]
+    },
+    {
+        name:"micheal j fox",
+        photo:"https://akm-img-a-in.tosshub.com/indiatoday/images/story/201401/michael_011314103857.jpg",
+        scores: [5,4,4,3,5,3,2,4,5,4]
+    }
+
+];
+
+module.exports = friends;
